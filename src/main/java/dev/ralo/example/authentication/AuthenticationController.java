ce53e37eafdd023ead46f58e20b95855b851ce2e
Raúl <ralo.dev@pm.me>
2023-08-05 00:20:54 -0600

    Added Authentication package
    • Added
        - Simple service authentication method with custom Exception logging
        - request and response records
        - rest controller to log in

---
+++ src/main/java/dev/ralo/example/authentication/AuthenticationController.java
@@ -0,0 +1,25 @@
+package dev.ralo.example.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public ResponseEntity<Object> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+        try {
+            return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
